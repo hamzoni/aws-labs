c4a5e462ce8eb007f6bfef66c73ef6cdc639a9be
taquy <taquy.se@gmail.com>
2021-01-10 21:35:05 +0700

    create cognito with iam role

--- aws-lab-serverless/cloudformation/create.sh
+++ aws-lab-serverless/cloudformation/create.sh
@@ -6,7 +6,17 @@ aws s3 sync . s3://aws-lab-cloudformation \
 TEMPLATE_URL=https://aws-lab-cloudformation.s3-us-west-2.amazonaws.com/entry.yml
 
 aws cloudformation create-stack \
-  --stack-name $1 \
-  --template-url $TEMPLATE_URL \
-  --region us-west-2 \
-  --profile pharos-cross
+	--capabilities CAPABILITY_IAM \
+	--stack-name $1 \
+	--template-body file://./entry.yml \
+	--region us-west-2 \
+	--profile pharos-cross
+
+  # --template-url $TEMPLATE_URL \
+
+
+# aws cloudformation create-stack \
+# 	--stack-name message-store \
+# 	--template-body file://bucket_with_keys.yaml \
+# 	--parameters file://cfg_bucket_with_keys.json \
+# 	--capabilities CAPABILITY_NAMED_IAM
